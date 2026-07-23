dda32c8d1fa519a92383416b8e919dc73f97d769
ayush7-singh <ayushsingh7504@gmail.com>
2026-07-23 12:02:29 +0530

    Time: 3 ms (85.56%), Space: 15.3 MB (53.98%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    int f(int ind, int target, vector<int>& nums) {
-        if (ind == 0) {
-            int ways = 0;
-            if (target == nums[0]) ways++;
-            if (target == -nums[0]) ways++;
-            return ways;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (sum < abs(target)) return 0;
+        if ((sum + target) % 2 != 0) return 0;
+
+        int k = (sum + target) / 2;
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(k + 1, 0));
+
+        // Base case
+        if (nums[0] == 0)
+            dp[0][0] = 2;          // +0 and -0
+        else
+            dp[0][0] = 1;
+
+        if (nums[0] != 0 && nums[0] <= k)
+            dp[0][nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= k; t++) {
+                int notTake = dp[i - 1][t];
+                int take = 0;
+                if (nums[i] <= t)
+                    take = dp[i - 1][t - nums[i]];
+
+                dp[i][t] = take + notTake;
+            }
         }
 
-        int plus = f(ind - 1, target - nums[ind], nums);
-        int minus = f(ind - 1, target + nums[ind], nums);
-
-        return plus + minus;
-    }
-
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums.size() - 1, target, nums);
+        return dp[n - 1][k];
     }
 };
